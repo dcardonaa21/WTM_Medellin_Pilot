d49d3317bcb85b9e4597e09868c2d0f93735031a
Daniela Cardona <dcardonaa2111@gmail.com>
2020-10-17 02:47:37 +0000

    [feat] Implementacion de la funcion download_data

--- STEM_Studio/stem_library/limpieza.py
+++ STEM_Studio/stem_library/limpieza.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import requests
 
 def data_as_df(data_path):
     data_dict = pd.read_excel(data_path, sheet_name=None)
@@ -17,3 +18,12 @@ def save_data_as_csv(data_path, save_path):
     for key in data_dict.keys():
         data_dict[key].to_csv(save_path / key + ".csv", index=False)
     return 
+
+def download_data(file_name, destination_folder):
+    # Define dónde se guardan los datos
+    # file_name: string que contiene el nombre del archivo con su extensión
+    # destination_folder: string que contiene la ruta relativa de la carpeta en la que se quiere guardar el archivo
+    r = requests.get(destination_folder+file_name, allow_redirects=True)
+    with open(destination_folder+file_name, 'wb') as f:
+        f.write(r.content)
+    return destination_folder+file_name
